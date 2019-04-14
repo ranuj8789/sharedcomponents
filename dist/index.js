beffcf16048606aa11d8eb7790207e20d4dd8593
Ranuj <ranuj8789@gmail.com>
2019-04-15 02:20:19 +0530

    hello

---
+++ dist/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactDom = require('react-dom');
+
+var _Textbox = require('./components/Textbox');
+
+var _Textbox2 = _interopRequireDefault(_Textbox);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+(0, _reactDom.render)(_react2.default.createElement(_Textbox2.default, null), document.getElementById("root"));
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import {render} from 'react-dom'
+
+
+import Grid from './components/Textbox';
+
+render(<Grid />, document.getElementById("root"));
